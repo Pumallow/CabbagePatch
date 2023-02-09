7eb30ea43e52e158927b5d5dff3a377f11a3cbbe
Pumallow <61199869+Pumallow@users.noreply.github.com>
2023-02-09 18:56:16 -0500

    Update Main.py

--- multipage_app/Main.py
+++ multipage_app/Main.py
@@ -3,8 +3,7 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 
-st.set_page_config(layout="wide")
-st.set_page_config(page_title="Marshal's Data Science")
+st.set_page_config(layout="wide", page_title="Marshal's Data Science")
 hide_default_format = """
        <style>
        #MainMenu {visibility: hidden; }
