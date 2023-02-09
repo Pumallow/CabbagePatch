c861ab94819d5d354dfe607164e1144c8dbd3389
Pumallow <61199869+Pumallow@users.noreply.github.com>
2023-02-09 17:18:26 -0500

    Update Main.py

--- multipage_app/Main.py
+++ multipage_app/Main.py
@@ -8,14 +8,12 @@ st.markdown("<h1 style='text-align: center; color: white;'>Marshal Turner's Data
 
 col1, col2 = st.columns([1,1])
 with col1:
-  st.markdown(
-    """<h1 style = 'text-align: left; color: white; font-size: 20px;'> Here are some works I have built along my journey as a data analyst and data scientist to prove/better my skills.
+  st.markdown("""<h1 style = 'text-align: left; color: white; font-size: 20px;'> Here are some works I have built along my journey as a data analyst and data scientist to prove/better my skills.
     I am a born and raised Georgian! Graduating from Kennesaw State & University with a degree in Industrial and Systems Engineering, I found myself with a fascination in tech.
     This ultimately led me to teach myself languages like SQL and Python as well as expose myself to software like Snowflake, Azure Databricks, Google BigQuery, PowerBi, Tableau, and more.
     Beyond the career drive, I spend my time elsewhere with friends or working on other activities such as soccer, working out, or playing the piano. I look to make the most
-    out of life which mostly entails laughs and jokes! The video below is one of many things I've created to mess with my friends.</h1>
-    
-    <h1 style = 'text-align: left; color: white; font-size: 20px;'>I hope to continually update this site with new content. Cheers! :D </h1>""")
+    out of life which mostly entails laughs and jokes! The video below is one of many things I've created to mess with my friends.</h1>""")
+  st.markdown("<h1 style = 'text-align: left; color: white; font-size: 20px;'>I hope to continually update this site with new content. Cheers! :D </h1>")
   st.markdown('If you are interested in learning more feel free to check out my [LinkedIn](https://www.linkedin.com/in/mturner95/).', unsafe_allow_html= True)
 with col2:
   pfp = Image.open("images/PFP.JPG")
