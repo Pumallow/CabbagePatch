76e0b0a1036e5bfe3ba128b6efd5069f478ecc64
Pumallow <61199869+Pumallow@users.noreply.github.com>
2023-02-09 17:12:26 -0500

    Update Main.py

--- multipage_app/Main.py
+++ multipage_app/Main.py
@@ -11,7 +11,12 @@ with col1:
   st.write(
     """
     Here are some works I have built along my journey as a data analyst and data scientist to prove/better my skills.
-    I hope to continually update this site with new content. Cheers :D
+    I am a born and raised Georgian! Graduating from Kennesaw State & University with a degree in Industrial and Systems Engineering, I found myself with a fascination in tech.
+    This ultimately led me to teach myself languages like SQL and Python as well as expose myself to software like Snowflake, Azure Databricks, Google BigQuery, PowerBi, Tableau, and more.
+    Beyond the career drive, I spend my time elsewhere with friends or working on other activities such as soccer, working out, or playing the piano. I look to make the most
+    out of life which mostly entails laughs and jokes! The video below is one of many things I've created to mess with my friends.
+    
+    I hope to continually update this site with new content. Cheers! :D
     """)
   st.markdown('If you are interested in learning more feel free to check out my [LinkedIn](https://www.linkedin.com/in/mturner95/).', unsafe_allow_html= True)
 with col2:
