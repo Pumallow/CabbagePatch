40fa1ae731b5838279d8ff40b654b631f4313e42
Pumallow <61199869+Pumallow@users.noreply.github.com>
2023-02-09 15:24:07 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,9 +8,8 @@ st.set_page_config(layout="wide")
 st.markdown("<h1 style='text-align: center; color: white;'>Marshal Turner's Data Science Website</h1>", unsafe_allow_html = True)
 st.write(
   """
-  Here are some works I have built along my journey as a data analyst and data scientist to prove/better my skills.
-  If you are interested in learning more feel free to check out my [LinkedIn] (https://www.linkedin.com/in/mturner95/).
-  I hope to continually update this site with new content. Cheers :D
-""")
+  Here are some works I have built along my journey as a data analyst and data scientist to prove/better my skills.""")
+st.markdown('If you are interested in learning more feel free to check out my [LinkedIn](https://www.linkedin.com/in/mturner95/).', unsafe_allow_html= True)
+st.write('I hope to continually update this site with new content. Cheers :D')
 st.sidebar.success("Pages to Peruse.")
 
