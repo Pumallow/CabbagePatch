b5d545912c5fe92772230f9e4de1fbb54f64101d
Pumallow <61199869+Pumallow@users.noreply.github.com>
2023-02-09 18:44:58 -0500

    Update Main.py

--- multipage_app/Main.py
+++ multipage_app/Main.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 
 st.set_page_config(layout="wide")
-st.markdown("<h1 style='text-align: center; color: black;'>Marshal Turner's Data Science Website</h1>", unsafe_allow_html = True)
+st.markdown("<h1 style='text-align: center; color: white;'>Marshal Turner's Data Science Website</h1>", unsafe_allow_html = True)
 vf =  open(r'images/troy.mov', 'rb')
 vb = vf.read()
 
@@ -14,12 +14,12 @@ side = max((100 - width) / 2, 0.01)
 
 col1, col2 = st.columns([5,2])
 with col1: 
-    st.markdown("""<h1 style= 'text-align: left; color: black; font-size: 20px;'> Here are some works I have built along my journey as a data analyst and data scientist to prove/better my skills.
+    st.markdown("""<h1 style= 'text-align: left; color: white; font-size: 20px;'> Here are some works I have built along my journey as a data analyst and data scientist to prove/better my skills.
       I am a born and raised Georgian! Graduating from Kennesaw State & University with a degree in Industrial and Systems Engineering, I found myself with a fascination in tech.
       This ultimately led me to teach myself languages like SQL and Python as well as expose myself to software like Snowflake, Azure Databricks, Google BigQuery, PowerBi, Tableau, and more.
      </h1>""", unsafe_allow_html= True) 
-    st.markdown("<h1 style= 'text-align: left; color: black; font-size: 20px;'> A menu for all my past projects is located on the left hand side of the screen (extend it by arrow in the top left corner). </h1>", unsafe_allow_html= True)
-    st.markdown("<h1 style= 'text-align: left; color: black; font-size: 20px;'>I hope to continually update this site with new content. Cheers! :D </h1>", unsafe_allow_html= True)
+    st.markdown("<h1 style= 'text-align: left; color: white; font-size: 20px;'> A menu for all my past projects is located on the left hand side of the screen (extend it by arrow in the top left corner). </h1>", unsafe_allow_html= True)
+    st.markdown("<h1 style= 'text-align: left; color: white; font-size: 20px;'>I hope to continually update this site with new content. Cheers! :D </h1>", unsafe_allow_html= True)
     st.markdown("If you are interested in connecting with me, feel free to check out my [LinkedIn](https://www.linkedin.com/in/mturner95/).", unsafe_allow_html= True)
   
 with col2:
@@ -29,7 +29,7 @@ col1, col2 = st.columns([2,5])
 with col1:
   st.video(data=vb)
 with col2:
-  st.markdown("""<h1 style= 'text-align: left; color: black; font-size: 20px;'> Beyond the career drive, I spend my time elsewhere with friends or working on other activities such as soccer, working out, or playing the piano. I look to make the most
+  st.markdown("""<h1 style= 'text-align: left; color: white; font-size: 20px;'> Beyond the career drive, I spend my time elsewhere with friends or working on other activities such as soccer, working out, or playing the piano. I look to make the most
   out of life which mostly entails laughs and jokes! The video below is one of many things I've created to mess with my friends.</h1>""", unsafe_allow_html= True) 
 
 
