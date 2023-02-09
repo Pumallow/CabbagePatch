582ff96a73e7093517f73cd62e703edc921b3bb8
Pumallow <61199869+Pumallow@users.noreply.github.com>
2023-02-09 15:52:24 -0500

    Update Main.py

--- multipage_app/Main.py
+++ multipage_app/Main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 
+pfp = Image.open(r'.\pics\PFP.jpg')
 st.set_page_config(layout="wide")
 st.markdown("<h1 style='text-align: center; color: white;'>Marshal Turner's Data Science Website</h1>", unsafe_allow_html = True)
 st.write(
@@ -10,5 +11,6 @@ st.write(
   Here are some works I have built along my journey as a data analyst and data scientist to prove/better my skills.""")
 st.markdown('If you are interested in learning more feel free to check out my [LinkedIn](https://www.linkedin.com/in/mturner95/).', unsafe_allow_html= True)
 st.write('I hope to continually update this site with new content. Cheers :D')
+st.image(pfp)
 st.sidebar.success("Pages to Peruse.")
 
