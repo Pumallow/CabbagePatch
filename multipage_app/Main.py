0a251fcae682cdf0ade88de3e92b8a57dc3f1ac5
Pumallow <61199869+Pumallow@users.noreply.github.com>
2023-02-09 16:15:08 -0500

    Update Main.py

--- multipage_app/Main.py
+++ multipage_app/Main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 
-pfp = Image.open("images/PFP.jpg")
+pfp = Image.open("images/PFP.jpeg")
 st.set_page_config(layout="wide")
 st.markdown("<h1 style='text-align: center; color: white;'>Marshal Turner's Data Science Website</h1>", unsafe_allow_html = True)
 st.write(
