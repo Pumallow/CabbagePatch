e55239acc745dc751ab774a8ec57b66431d23f94
Pumallow <61199869+Pumallow@users.noreply.github.com>
2023-02-09 18:12:35 -0500

    Update Main.py

--- multipage_app/Main.py
+++ multipage_app/Main.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 
-showWarningOnDirectExecution = true
-
 st.set_page_config(layout="wide")
 st.markdown("<h1 style='text-align: center; color: white;'>Marshal Turner's Data Science Website</h1>", unsafe_allow_html = True)
 vf =  open(r'images/troy.mov', 'rb')
@@ -14,7 +12,7 @@ width = 50
 width = max(width, 0.01)
 side = max((100 - width) / 2, 0.01)
 
-col1, col2 = st.beta_columns([5,2])
+col1, col2 = st.columns([5,2])
 with col1: 
     st.markdown("""<h1 style= 'text-align: left; color: white; font-size: 20px;'> Here are some works I have built along my journey as a data analyst and data scientist to prove/better my skills.
       I am a born and raised Georgian! Graduating from Kennesaw State & University with a degree in Industrial and Systems Engineering, I found myself with a fascination in tech.
@@ -27,7 +25,7 @@ with col1:
 with col2:
   pfp = Image.open("images/PFP.JPG")
   st.image(pfp)
-col1, col2 = st.beta_columns([2,5])
+col1, col2 = st.columns([2,5])
 with col1:
   st.video(data=vb)
 with col2:
