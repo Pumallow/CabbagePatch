0742ab807c86f5cb53a3f7d320dd2fd4bdb0108f
Pumallow <61199869+Pumallow@users.noreply.github.com>
2023-02-10 10:01:29 -0500

    Update 1_Early Projects.py

--- multipage_app/pages/1_Early Projects.py
+++ multipage_app/pages/1_Early Projects.py
@@ -13,7 +13,7 @@ st.set_page_config(layout="wide", page_title= "Data Analytics/Science Journey")
 st.markdown("<h1 style= 'text-align: center; color: white; font-size: 40px' >Projects that Segway into Data Analytics</h1>", unsafe_allow_html = True)
 st.sidebar.success("Pages to Peruse")
 st.markdown("""June, 2021 <br>
-This project helped me land my first Data Analyst job at NTG. I combined my love for soccer with my fascination for data by web scraping 2400 + pages of data on the [Premier League Website] (https://www.premierleague.com/stats/top/players/goals?se=79) in python, grooming the gathered data in my local SQL host, then presenting an interactive Power BI Dashboard. Its purpose is to highlight the best left footed players/teams vs the best right footed players/teams.""", unsafe_allow_html= True)
+This project helped me land my first Data Analyst job at NTG. I combined my love for soccer with my fascination for data by web scraping 2400 + pages of data on the Premier League Website in python, grooming the gathered data in my local SQL host, then presenting an interactive Power BI Dashboard. Its purpose is to highlight the best left footed players/teams vs the best right footed players/teams.""", unsafe_allow_html= True)
 st.markdown("[ETL Project](https://www.linkedin.com/feed/update/urn:li:activity:6821946098987401216/)", unsafe_allow_html= True)
 st.markdown("""July, 2021 <br>
 I took the ETL Project one step further by portraying the Transformation phases of the ETL Process with Alteryx after taking an introductory course.""", unsafe_allow_html= True)
