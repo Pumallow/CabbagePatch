99a91aad8a9dd25e76551edc314d9810670fdfb8
Pumallow <61199869+Pumallow@users.noreply.github.com>
2023-02-11 16:01:07 -0500

    Update Main.py

--- multipage_app/Main.py
+++ multipage_app/Main.py
@@ -51,8 +51,8 @@ col1, col2 = st.columns([2,5])
 with col1:
   st.video(data=vb)
 with col2:
-  st.markdown("""<h1 style= 'text-align: left; font-size: 20px;'> Beyond the career drive, I spend my time elsewhere with friends or working on other activities such as soccer, working out, or playing the piano. I look to make the most
-  out of life which mostly entails laughs and jokes! The video below is one of many things I've created to mess with my friends.</h1>""", unsafe_allow_html= True) 
+  st.markdown("""<h1 style= 'text-align: left; font-size: 20px;'> Beyond the career drive, I spend my time elsewhere with friends or other doing other activities like soccer, working out, or playing the piano. I look to make the most
+  out of life which mostly entails laughs and jokes! The video to the left is one of many things I've created to mess with my friends.</h1>""", unsafe_allow_html= True) 
 
 
 
