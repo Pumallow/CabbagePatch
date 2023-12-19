34566230c174151e0b3af866e1fa1c116d418d1e
Pumallow <61199869+Pumallow@users.noreply.github.com>
2023-12-19 10:11:05 -0500

    Update Main.py

--- multipage_app/Main.py
+++ multipage_app/Main.py
@@ -57,7 +57,6 @@ with col1:
     Machine Learning A-Z: Hands-On Python & R in Data Science <br>
     Data Science in Layman’s Terms: Time Series Analysis <br>""", unsafe_allow_html= True)
     st.write("""<h1 style= 'text-align: center; font-size: 35px;'>EDX Course Certifications </h1>
-    <h1 style= 'text-align: left; font-size: 20px;'>The Complete SQL Bootcamp by Jose Portilla <br>
     Finished EDX - SU23: Introduction to Analytics Modeling (Score: 84) <br>
     Finished EDX - Data Analytics for Business (Score: 86) <br>
     Finished EDX - FA23: Computing for Data Analysis (Score: 94) </h1>""", unsafe_allow_html= True)
