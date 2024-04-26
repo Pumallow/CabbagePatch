415a79dfb0b0281638f2eb7494ca50c1165d99fc
Pumallow <61199869+Pumallow@users.noreply.github.com>
2024-04-26 16:31:53 -0400

    Update 2_Bottega_Louie_Sentiment_Analysis.py

--- multipage_app/pages/2_Bottega_Louie_Sentiment_Analysis.py
+++ multipage_app/pages/2_Bottega_Louie_Sentiment_Analysis.py
@@ -43,9 +43,9 @@ In essence, the filtering turns reviews into a latin-esque fashion.""", unsafe_a
 
 col1, col2 = st.columns([1,1])
 with col1:
-       st.markdown(container_style, unsafe_allow_html=True)
-       st.markdown("""<svg style = 'border-radius: 8px'; 'border: 2px solid #FFFFFF';> <div style = 'font-size: 20px'>Before the transformation:</div>
-       <div>Beautiful restaurant and always delicious food! I always enjoy the Carbonara- my favorite. But pizzas have been great as well as their Pomodoro pasta."</div></svg>""", unsafe_allow_html=True) 
+       with st.container():
+              st.markdown("""<div style = 'font-size: 20px'>Before the transformation:</div>
+              <div>Beautiful restaurant and always delicious food! I always enjoy the Carbonara- my favorite. But pizzas have been great as well as their Pomodoro pasta."</div>""", unsafe_allow_html=True) 
 with col2:
        st.markdown(container_style, unsafe_allow_html=True)
        st.markdown("<div style = 'font-size: 20px'>After the transformation:" , unsafe_allow_html=True) 
