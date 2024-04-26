2e670839cd30aaab37f63c6a2467ae863f464656
Pumallow <61199869+Pumallow@users.noreply.github.com>
2024-04-26 16:18:12 -0400

    Update 2_Bottega_Louie_Sentiment_Analysis.py

--- multipage_app/pages/2_Bottega_Louie_Sentiment_Analysis.py
+++ multipage_app/pages/2_Bottega_Louie_Sentiment_Analysis.py
@@ -39,11 +39,11 @@ st.markdown("""For our classifier to best consume each description, NLTK and Skl
 sentiments, are filtered out. I.E. prepositional phrases, articles, or certain verbage \n
 3. Groupings of root words and their conjugations are then made. I.E. [Running, Runs, Runner, Ran] -> run \n
 \n
-In essence, the filtering turns reviews into a latin-esque fashion""", unsafe_allow_html= True)
+In essence, the filtering turns reviews into a latin-esque fashion.""", unsafe_allow_html= True)
 
-st.markdown("<div style = 'font-size: 20px'>Before the transformation:", unsafe_allow_html=True) 
+st.markdown("<div style = 'text-align: center;font-size: 20px'>Before the transformation:", unsafe_allow_html=True) 
 st.markdown('"Beautiful restaurant and always delicious food! I always enjoy the Carbonara- my favorite. But pizzas have been great as well as their Pomodoro pasta."', unsafe_allow_html=True) 
-st.markdown("<div style = 'font-size: 20px'>After the transformation:" , unsafe_allow_html=True) 
+st.markdown("<div style = 'text-align: center;font-size: 20px'>After the transformation:" , unsafe_allow_html=True) 
 st.markdown('"beauti restaur alway delici food alway enjoy carbonara favorit pizza great well pomodoro pasta"', unsafe_allow_html= True)
 
 
