d80ddec63d30b78f3231992f328df00f19225af4
Pumallow <61199869+Pumallow@users.noreply.github.com>
2026-05-01 12:39:25 -0400

    Update 1_Ronaldo_LLM_RAG_Architecture.py

--- multipage_app/pages/1_Ronaldo_LLM_RAG_Architecture.py
+++ multipage_app/pages/1_Ronaldo_LLM_RAG_Architecture.py
@@ -9,9 +9,7 @@ import time
 from dotenv import load_dotenv
 load_dotenv()
 
-project_root = Path(__file__).resolve().parents[3]
-sys.path.append(str(project_root))
-from llm.llm import get_cr7_response
+from multipage_app.pages.llm.llm import get_cr7_response
 # ====================== PAGE CONFIG & STYLING ======================
 st.set_page_config(
     page_title="CR7FanBot ⚽",
